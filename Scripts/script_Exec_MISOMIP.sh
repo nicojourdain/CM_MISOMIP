0824d25f629622695e65d31b7a71395f6e24c38c
imerino <imerino@login2.occigen>
2017-05-05 16:46:48 +0200

    newScriptExec.sh

--- Scripts/script_Exec_MISOMIP.sh
+++ Scripts/script_Exec_MISOMIP.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
 run=<run>
 RUN_NEMO=<NEMO_RUN>
 RUN_ELMER=<ELMER_RUN>
 
-cd $RUN_ELMER
-# Options: 1 execute NEMO after init Domain, 
-#	   0 only initilize Elmer
-./scriptInitDomain.sh 1
+VAL=$1
 
+
+LINE_NRUN=$(grep -n 'NRUN_MAX=' $RUN_ELMER/scriptIce1rExecute.sh | grep -Eo '^[^:]+')
+STRING="NRUN_MAX="$VAL
+
+awk 'NR=='$LINE_NRUN' {$0="'${STRING}'"} 1' $RUN_ELMER/scriptIce1rExecute.sh > test.sh
+chmod 755 test.sh 
+mv test.sh $RUN_ELMER/scriptIce1rExecute.sh
+
+cd $RUN_NEMO
+
+sbatch ./run_nemo_ISOMIP.sh 0 'DUMMY_FILE'
