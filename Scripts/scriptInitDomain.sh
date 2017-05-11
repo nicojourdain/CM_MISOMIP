2b11ff0a70998aeea80851a61d30a43c5ee52a20
imerino <imerino@login3.occigen>
2017-05-11 14:58:49 +0200

    bug init

--- Scripts/scriptInitDomain.sh
+++ Scripts/scriptInitDomain.sh
@@ -25,6 +25,7 @@ MeshPath=<MeshNamePath>
 
 mkdir -p $MeshPath/Results/$name
 
+rm -r $MeshPath/RESTART
 ln -sf $PATH_RESTART/$caseTest/Results/$nameRestart $MeshPath/RESTART
 echo $PATH_RESTART/$caseTest/Results/$nameRestart
 Restart=../RESTART/$nameRestart.result
@@ -67,5 +68,5 @@ cat $slurmScketch | sed -e "s#<jobName>#$jobName#g" \
 START_FROM_RST=$2
 RST_FILE=$3
 
-sbatch $slurmFile 1 $CALL_NEMO "DUMMY_MELT_FILE" $START_FROM_RST $RST_FILE
+sbatch $slurmFile 1 $CALL_NEMO "DUMMY_MELT_FILE" $START_FROM_RST $RST_FILE 1
 
