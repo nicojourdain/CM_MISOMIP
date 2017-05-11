08cb36bc3c28dc7751d4f620a81084ccb2b8bae1
imerino <imerino@login2.occigen>
2017-05-11 17:18:34 +0200

    rm RUN

--- RUNS/PRUEBA/isf_draft_meter.nc
+++
@@ -1,1 +0,0 @@
-/scratch/cnt0021/gge6066/imerino/NEMO_MISOMIP/run/PRUEBA/ISF_DRAFT_FROM_ELMER/isf_draft_meter_6.nc
--- RUNS/TOTO/script_Exec_MISOMIP.sh
+++
@@ -1,9 +0,0 @@
-run=TOTO
-RUN_NEMO=/scratch/cnt0021/gge6066/imerino/NEMO_MISOMIP/run/TOTO
-RUN_ELMER=/scratch/cnt0021/gge6066/imerino/ELMER_MISOMIP/TOTO/Work
-
-cd $RUN_ELMER
-# Options: 1 execute NEMO after init Domain, 
-#	   0 only initilize Elmer
-./scriptInitDomain.sh 1
-
