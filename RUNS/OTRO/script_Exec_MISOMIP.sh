ec771125b6465fa6aed05d9f66ea9bfe9afe2114
imerino <imerino@login3.occigen>
2017-05-15 14:59:24 +0200

    rm RUNS

--- RUNS/OTRO/script_Exec_MISOMIP.sh
+++
@@ -1,9 +0,0 @@
-run=OTRO
-RUN_NEMO=/scratch/cnt0021/gge6066/imerino/NEMO_MISOMIP/run/OTRO
-RUN_ELMER=/scratch/cnt0021/gge6066/imerino/ELMER_MISOMIP/OTRO/Work
-
-cd $RUN_ELMER
-# Options: 1 execute NEMO after init Domain, 
-#	   0 only initilize Elmer
-./scriptInitDomain.sh 1
-
